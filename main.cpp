e7b6e90906070ecb0e8812044b71c602c47a09d9
beginnerSC <cc12s@my.fsu.edu>
2023-02-20 00:36:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,6 @@
 // https://devblogs.microsoft.com/cppblog/using-github-codespaces-and-visual-studio-code-for-cplusplus-development/
 // https://github.com/dassencio/codespaces-demo-cpp
+
+// cmake in codespace
+    // https://www.youtube.com/watch?v=D8w3L-Oe8AM
+    // https://github.com/nickuhlenhuth/cmake
