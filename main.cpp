5e65e684616b0a288d86fb46ada6af333cc01e15
beginnerSC <cc12s@my.fsu.edu>
2023-02-20 00:43:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,6 @@
 // cmake in codespace
     // https://www.youtube.com/watch?v=D8w3L-Oe8AM
     // https://github.com/nickuhlenhuth/cmake
+    // https://github.com/nickuhlenhuth/cmake/blob/master/.devcontainer/devcontainer.json
+    // https://code.visualstudio.com/docs/devcontainers/containers#_getting-started
+    // https://code.visualstudio.com/docs/devcontainers/containers#_quick-start-try-a-development-container
